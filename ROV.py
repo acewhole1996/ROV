242b49eaed1f67a99e908fd1a594776c14230451
ace whole <50895028+acewhole1996@users.noreply.github.com>
2024-06-21 20:50:13 +0800

    changes stuff

--- ROV.py
+++ ROV.py
@@ -18,7 +18,7 @@ PAGE = """\
     margin: 0;
     padding: 0;
     height: 100vh;
-    overflow: hidden; /* Hide any scrollbars */
+    overflow: hidden; /* Hide any scrollbarsxaaaa */
   }
   #video-stream {
     width: 100vw;
