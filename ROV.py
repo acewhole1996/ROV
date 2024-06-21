064bcb7dbe6e2f6f713c55677eafb398c6dab03b
ace whole <50895028+acewhole1996@users.noreply.github.com>
2024-06-21 20:52:08 +0800

    sasasa

--- ROV.py
+++ ROV.py
@@ -18,7 +18,7 @@ PAGE = """\
     margin: 0;
     padding: 0;
     height: 100vh;
-    overflow: hidden; /* Hide any scrollbarsxaaa */
+    overflow: hidden; /* Hide any scrollbars */
   }
   #video-stream {
     width: 100vw;
